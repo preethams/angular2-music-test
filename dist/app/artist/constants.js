20ed60163c8abdd2a41dbea882fdfd4a52b05f2f
Preetham Shrinivas <pshrinivas@shutterfly.com>
2016-04-15 01:37:51 -0700

    trying some prod builds

--- dist/app/artist/constants.js
+++ dist/app/artist/constants.js
@@ -1,12 +1,1 @@
-System.register([], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var songsRoot;
-    return {
-        setters:[],
-        execute: function() {
-            exports_1("songsRoot", songsRoot = '/songs/');
-        }
-    }
-});
-//# sourceMappingURL=constants.js.map
+System.register([],function(exports_1,context_1){"use strict";var __moduleName=context_1&&context_1.id;var songsRoot;return{setters:[],execute:function(){exports_1("songsRoot",songsRoot="/songs/")}}});
